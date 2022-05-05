8635906a1e7075c74d1d0390bf75673ece03a9eb
unknown <Pobarul-islam>
2022-05-05 20:25:01 +0600

    insert data to database

--- index.js
+++ index.js
@@ -18,17 +18,20 @@ app.use(express.json());
 
 
 
-
-const uri = "mongodb+srv://dbuser1:<password>@cluster0.qd4rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qd4rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 
+async function run() {
+    try {
+       
+    }
+
+    finally {
+       
+    }
+}
+
+run().catch(console.dir)
 
 app.get('', (req, res) => {
     res.send('Assingment-11 Server is Running')
