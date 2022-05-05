f70a2a9964c2f699380e07ae4e7e08443aff5d2b
unknown <Pobarul-islam>
2022-05-05 16:54:28 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('', (req, res) => {
     res.send('Assingment-11 Server is Running')
