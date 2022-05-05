0262b2977ea9fee9849381c854e4749799144e78
unknown <Pobarul-islam>
2022-05-05 16:52:46 +0600

    assingent 11 server side running

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('', (req, res) => {
+    res.send('Assingment-11 Server is Running')
+});
+
+app.listen(port, () => {
+    console.log('Assingment-11 Server is Running')
+})
