8b84c3474da4b38ca0709bafdb2ceac505783673
unknown <Pobarul-islam>
2022-05-05 21:33:58 +0600

    env file create and some modified

--- index.js
+++ index.js
@@ -18,25 +18,42 @@ app.use(express.json());
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qd4rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('watche stocke server running')
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 async function run() {
     try {
-       
+        // await client.connect();
+        // const watchCollection = client.db('stockWatches').collection('ourWatches');
+
+        // app.get('/service', async (req, res) => {
+        //     const query = {};
+        //     const cursor = watchCollection.find(query);
+        //     const services = await cursor.toArray();
+        //     res.send(services);
+        // })
     }
 
     finally {
-       
+
     }
 }
 
 run().catch(console.dir)
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Assingment-11 Server is Running')
 });
 
 app.listen(port, () => {
-    console.log('Assingment-11 Server is Running')
+    console.log('Assingment-11 Server is Running',port)
 })
