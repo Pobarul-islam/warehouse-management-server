3adbabfcbdacffaa1b0ba55f9e23536228e4339b
unknown <Pobarul-islam>
2022-05-07 20:52:52 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -35,6 +35,14 @@ const run = async () => {
             const coursor = watchesCollection.find(query);
             const watches = await coursor.toArray();
             res.send(watches);
+        });
+
+        // Delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
         })
 
     }
